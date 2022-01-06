c948bf883802c5942f72cbd7174ca0d676e5c45f
Alex Kim <alex@ajpkim.com>
2022-01-06 09:01:59 -0500

    update external monitor script

--- turn-on-monitors.sh
+++ turn-on-monitors.sh
@@ -2,8 +2,8 @@
 
 # External monitors 2021-11
 M0="eDP1"
-M1="DP2-2"
-M1_mode="1920x1080"
+M1="DP2-1"
+M1_mode="3840x2160"
 M2="HDMI1"
 M2_mode="3840x2160"
 
@@ -11,5 +11,5 @@ M2_mode="3840x2160"
 . ~/scripts/turn-off-monitors.sh
 
 ## Setup external monitors.
-xrandr --output $M1 --mode $M1_mode --right-of $M0
-xrandr --output $M2 --mode $M2_mode --right-of $M1
+xrandr --output $M1 --mode $M1_mode --right-of $M0 --set audio off
+xrandr --output $M2 --mode $M2_mode --right-of $M1 --set audio off  --rotate right
