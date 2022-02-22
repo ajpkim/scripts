e7e0ba852abf296c7270691c315dfa709e4965f8
Alex Kim <alex@ajpkim.com>
2022-02-22 13:29:28 -0500

    Update org cronjob script for cabcs repo

--- commit-org.sh
+++ commit-org.sh
@@ -1,2 +1,2 @@
 cd ~/org && /usr/bin/git add --all && /usr/bin/git commit -m "Daily update"
-cd ~/org && /usr/bin/git push ak-git main && /usr/bin/git push github main
+cd ~/org && /usr/bin/git push origin main
